48373b9a29d0ffc0c324071515e11eeef3363c8d
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-12 21:31:02 +0900

    add TDD example

--- src/components/Single.test.jsx
+++ src/components/Single.test.jsx
@@ -1,1 +1,13 @@
 /* eslint-disable no-undef */
+
+// function to be test
+const countLetters = (data) => data.length;
+
+// TDD test driven development
+describe('TDD example -- write the test first, then write the code to pass the test', () => {
+  const data = 'abc';
+  it('count letters correctly', () => {
+    const results = countLetters(data);
+    expect(results).toEqual(3);
+  });
+});
