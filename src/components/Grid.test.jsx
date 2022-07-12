7cba9b168e0b975a67abe227fdadd4d7f7768778
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-12 20:05:08 +0900

    add UI snapshot test

--- src/components/Grid.test.jsx
+++ src/components/Grid.test.jsx
@@ -2,6 +2,7 @@
 
 import data from '../data/dummy.json';
 
+// Test numbers
 const numItems = data.length;
 
 test('Number of items = 5', () => {
@@ -10,4 +11,26 @@ test('Number of items = 5', () => {
 
 test('Number of items = 5', () => {
   expect(numItems).toBeGreaterThanOrEqual(5);
+});
+
+// Test string
+const dataTest = data[0].title;
+
+test('title contains jack', () => {
+  expect(dataTest).toMatch(/jack/);
+});
+
+test('title contains jack', () => {
+  expect(dataTest).toContain('jack');
+});
+
+// Test items in array
+const dataArray = ['fun', 'jack'];
+test('title list contains jack', () => {
+  expect(['fun', 'jack', 'tom']).toEqual(expect.arrayContaining(dataArray));
+});
+
+//  Test Object
+test('title list contains jack', () => {
+  expect(data[0]).toHaveProperty('title');
 });
