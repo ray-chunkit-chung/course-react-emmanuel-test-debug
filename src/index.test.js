cb4801ae909ecbb4e9e65fe72838278ac82acacd
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-13 06:09:59 +0900

    minor text change

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 
 // import index from './index.jsx';
-import App from './App';
+// import App from './App';
 
 // Mock the App. This is for test app loading by not using the true App
-jest.mock('./App');
+// jest.mock('./App');
 
-// this test is failed.
+// this test suite is failed.
 describe('App load correctly', () => {
   // it('should call app.listen()', () => {
   //   expect(App.listen).toHaveBeenCalled();
