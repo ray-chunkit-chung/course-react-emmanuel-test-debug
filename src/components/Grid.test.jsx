de6d34ff55d73fa65747b6ea31cd99e76658488c
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2022-07-12 20:12:42 +0900

    add test describe block

--- src/components/Grid.test.jsx
+++ src/components/Grid.test.jsx
@@ -2,35 +2,39 @@
 
 import data from '../data/dummy.json';
 
-// Test numbers
 const numItems = data.length;
-
-test('Number of items = 5', () => {
-  expect(numItems).toBe(5);
-});
-
-test('Number of items = 5', () => {
-  expect(numItems).toBeGreaterThanOrEqual(5);
-});
-
-// Test string
 const dataTest = data[0].title;
 
-test('title contains jack', () => {
-  expect(dataTest).toMatch(/jack/);
+describe('num and string', () => {
+// Test numbers
+  test('Number of items = 5', () => {
+    expect(numItems).toBe(5);
+  });
+
+  test('Number of items = 5', () => {
+    expect(numItems).toBeGreaterThanOrEqual(5);
+  });
+
+  // Test string
+
+  test('title contains jack', () => {
+    expect(dataTest).toMatch(/jack/);
+  });
+
+  test('title contains jack', () => {
+    expect(dataTest).toContain('jack');
+  });
 });
 
-test('title contains jack', () => {
-  expect(dataTest).toContain('jack');
-});
+describe('array and object', () => {
+  // Test items in array
+  const dataArray = ['fun', 'jack'];
+  test('title list contains jack', () => {
+    expect(['fun', 'jack', 'tom']).toEqual(expect.arrayContaining(dataArray));
+  });
 
-// Test items in array
-const dataArray = ['fun', 'jack'];
-test('title list contains jack', () => {
-  expect(['fun', 'jack', 'tom']).toEqual(expect.arrayContaining(dataArray));
-});
-
-//  Test Object
-test('title list contains jack', () => {
-  expect(data[0]).toHaveProperty('title');
+  //  Test Object
+  test('title list contains jack', () => {
+    expect(data[0]).toHaveProperty('title');
+  });
 });
